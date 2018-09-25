021106e70d29eb116d7986aa078c204b65c42ef5
afrizalnursalim <43517341+afrizalnursalim@users.noreply.github.com>
2018-09-25 16:56:59 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,43 @@
-<?php
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Form Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <br/>
+  <br/>
+  <center><h2>LOGIN</h2>  
+  <br/>
+  <div class="login">
+  <br/>
+    <form action="proses.php" method="post" onSubmit="return validasi()">
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" name="password" id="password" />
+      </div>      
+      <div>
+        <input type="submit" value="Login" class="tombol">
+      </div>
+    </form>
+  </div>
+</body>
+ </CENTER>
+<script type="text/javascript">
+  function validasi() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;   
+    if (username != "" && password!="") {
+      return true;
+    }else{
+      alert('Username dan Password harus di isi !');
+      return false;
+    }
+  }
+ 
+</script>
+</html>
